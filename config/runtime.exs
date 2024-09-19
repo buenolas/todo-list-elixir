193367585a01ae6557b734d5e87295a47cb183ba
Lucas Bueno <lucasdealmeidabueno@gmail.com>
2024-09-19 14:46:10 -0300

    Update runtime.exs

--- config/runtime.exs
+++ config/runtime.exs
@@ -49,7 +49,7 @@ if config_env() == :prod do
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :todo_list, TodoListWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: host, port: 4000],
     http: [
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
